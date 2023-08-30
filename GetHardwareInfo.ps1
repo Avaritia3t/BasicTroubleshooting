478e560866d77b21a7d9c6b706ce71ef2b398777
Avaritia3t <90818291+Avaritia3t@users.noreply.github.com>
2023-08-29 23:28:03 -0700

    Update GetHardwareInfo.ps1
    Improved scoping and error checking.

--- GetHardwareInfo.ps1
+++ GetHardwareInfo.ps1
@@ -1,21 +1,20 @@
 # Script written for PowerShell 7
-
-function Get-HardwareInfo {
-    if ($PSVersionTable.PSVersion.Major -lt 7) {
+if ($PSVersionTable.PSVersion.Major -lt 7) {
         Write-Error "This script requires PowerShell 7 or higher."
         exit
     }
 
-    # Initialize the report array
-    $hardwareReport = @()
+# Initialize the report array
+$hardwareReport = @()
 
+function Get-HardwareInfo {
+    
     # Function to get hardware make and model
     function Get-SystemMakeModel {
         Write-Host "Retrieving system make and model..." -ForegroundColor Yellow
         $systemInfo = Get-CimInstance -ClassName Win32_ComputerSystem
         $makeModelMessage = "System Manufacturer: $($systemInfo.Manufacturer), Model: $($systemInfo.Model)"
-        $hardwareReport += $makeModelMessage
-        Write-Host $makeModelMessage
+        $script:hardwareReport += $makeModelMessage
         Write-Host "System make and model retrieval completed." -ForegroundColor Green
     }
 
@@ -24,8 +23,7 @@ function Get-HardwareInfo {
         Write-Host "Retrieving processor information..." -ForegroundColor Yellow
         $processor = Get-CimInstance -ClassName Win32_Processor
         $processorMessage = "Processor: $($processor.Name)"
-        $hardwareReport += $processorMessage
-        Write-Host $processorMessage
+        $script:hardwareReport += $processorMessage
         Write-Host "Processor information retrieval completed." -ForegroundColor Green
     }
 
@@ -34,8 +32,7 @@ function Get-HardwareInfo {
         Write-Host "Retrieving BIOS version..." -ForegroundColor Yellow
         $bios = Get-CimInstance -ClassName Win32_BIOS
         $biosMessage = "BIOS Version: $($bios.SMBIOSBIOSVersion)"
-        $hardwareReport += $biosMessage
-        Write-Host $biosMessage
+        $script:hardwareReport += $biosMessage
         Write-Host "BIOS version retrieval completed." -ForegroundColor Green
     }
 
@@ -45,8 +42,7 @@ function Get-HardwareInfo {
         $memory = Get-CimInstance -ClassName Win32_PhysicalMemory
         foreach ($mem in $memory) {
             $memoryMessage = "Memory Stick Capacity: $($mem.Capacity / 1GB)GB, Speed: $($mem.Speed)MHz, Manufacturer: $($mem.Manufacturer)"
-            $hardwareReport += $memoryMessage
-            Write-Host $memoryMessage
+            $script:hardwareReport += $memoryMessage
         }
         Write-Host "Memory information retrieval completed." -ForegroundColor Green
     }
@@ -57,8 +53,7 @@ function Get-HardwareInfo {
         $disks = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3"
         foreach ($disk in $disks) {
             $diskMessage = "Disk: $($disk.DeviceID), Size: $($disk.Size / 1GB)GB, Free Space: $($disk.FreeSpace / 1GB)GB"
-            $hardwareReport += $diskMessage
-            Write-Host $diskMessage
+            $script:hardwareReport += $diskMessage
         }
         Write-Host "Disk drive information retrieval completed." -ForegroundColor Green
     }
@@ -69,8 +64,7 @@ function Get-HardwareInfo {
         $graphics = Get-CimInstance -ClassName Win32_VideoController
         foreach ($card in $graphics) {
             $graphicsMessage = "Graphics Card: $($card.Name), Memory: $($card.AdapterRAM / 1MB)MB"
-            $hardwareReport += $graphicsMessage
-            Write-Host $graphicsMessage
+            $script:hardwareReport += $graphicsMessage
         }
         Write-Host "Graphics card information retrieval completed." -ForegroundColor Green
     }
@@ -81,8 +75,7 @@ function Get-HardwareInfo {
         $adapters = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled }
         foreach ($adapter in $adapters) {
             $adapterMessage = "Network Adapter: $($adapter.Description), IP Address: $($adapter.IPAddress -join ', ')"
-            $hardwareReport += $adapterMessage
-            Write-Host $adapterMessage
+            $script:hardwareReport += $adapterMessage
         }
         Write-Host "Network adapter information retrieval completed." -ForegroundColor Green
     }
@@ -93,8 +86,7 @@ function Get-HardwareInfo {
         $battery = Get-CimInstance -ClassName Win32_Battery
         if ($battery) {
             $batteryMessage = "Battery Status: $($battery.Status), Estimated Charge Remaining: $($battery.EstimatedChargeRemaining)%"
-            $hardwareReport += $batteryMessage
-            Write-Host $batteryMessage
+            $script:hardwareReport += $batteryMessage
         } else {
             Write-Host "No battery detected on this device." -ForegroundColor Cyan
         }
