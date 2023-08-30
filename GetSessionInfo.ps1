47794a65bdf57c7ceb59a2fcfdca77e9fe2315e8
Avaritia3t <90818291+Avaritia3t@users.noreply.github.com>
2023-08-29 23:28:51 -0700

    Update GetSessionInfo.ps1
    Improved scoping, updated powershell check location.

--- GetSessionInfo.ps1
+++ GetSessionInfo.ps1
@@ -1,23 +1,23 @@
-﻿# GetSessionInfo.ps1
+# GetSessionInfo.ps1
 # Please note this script must be run with elevated permissions
 # Script written for PowerShell 7
 
-function Get-SessionInfo {
-    if ($PSVersionTable.PSVersion.Major -lt 7) {
+if ($PSVersionTable.PSVersion.Major -lt 7) {
         Write-Error "This script requires PowerShell 7 or higher."
         exit
-    }
+    }    
 
-    # Initialize the report array
-    $sessionReport = @()
+# Initialize the report array
+$sessionReport = @()
+
+function Get-SessionInfo {
 
     # Function to retrieve the current username
     function Get-Username {
         Write-Host "Retrieving current username..." -ForegroundColor Yellow
         $username = $env:USERNAME
         $usernameMessage = "Current Username: $username"
-        $sessionReport += $usernameMessage
-        Write-Host $usernameMessage
+        $script:sessionReport += $usernameMessage
         Write-Host "Username retrieval completed." -ForegroundColor Green
     }
 
@@ -26,8 +26,7 @@ function Get-SessionInfo {
         Write-Host "Retrieving last boot time..." -ForegroundColor Yellow
         $lastBootTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
         $bootTimeMessage = "Last Boot Time: $lastBootTime"
-        $sessionReport += $bootTimeMessage
-        Write-Host $bootTimeMessage
+        $script:sessionReport += $bootTimeMessage
         Write-Host "Last boot time retrieval completed." -ForegroundColor Green
     }
 
@@ -50,7 +49,6 @@ function Get-SessionInfo {
     if ($lastLogin) {
         $loginTimeMessage = "Last Session Login Time: $($lastLogin.TimeCreated)"
         $script:sessionReport += $loginTimeMessage
-        Write-Host $loginTimeMessage
     } else {
         Write-Host "No interactive logon events found." -ForegroundColor Yellow
     }
@@ -63,11 +61,10 @@ function Get-SessionInfo {
         Write-Host "Retrieving OS version and build..." -ForegroundColor Yellow
         $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
         $osInfoMessage = "OS Version: $($osInfo.Version) | Build: $($osInfo.BuildNumber)"
-        $sessionReport += $osInfoMessage
-        Write-Host $osInfoMessage
+        $script:sessionReport += $osInfoMessage
         Write-Host "OS version and build retrieval completed." -ForegroundColor Green
     }
-
+    
     # Function to check for for most recent OS updates
     function Check-OSUpdates {
         Write-Host "Checking for OS updates..." -ForegroundColor Yellow
@@ -81,10 +78,10 @@ function Get-SessionInfo {
             foreach ($update in $recentUpdates) {
                 $updateMessages += "HotFixID: $($update.HotFixID), Installed On: $($update.InstalledOn)"
             }
-            $sessionReport += $updateMessages
+            $script:sessionReport += $updateMessages
         } else {
             $updateMessage = "No OS updates found."
-            $sessionReport += $updateMessage
+            $script:sessionReport += $updateMessage
             Write-Host $updateMessage -ForegroundColor Red
         }
 
@@ -100,7 +97,5 @@ function Get-SessionInfo {
     Check-OSUpdates
 
     # Return the report for use in BasicTroubleshooting Script
-    Write-Host $sessionReport
     return $sessionReport
 }
-
